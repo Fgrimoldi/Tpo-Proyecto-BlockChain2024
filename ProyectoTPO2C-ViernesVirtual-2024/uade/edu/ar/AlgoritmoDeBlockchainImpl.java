9589d80f2af4a9d676bd2af80cd995e0b6d6a397
Fgrimoldi <grimoldifacundo@gmail.com>
2024-11-14 20:02:53 -0300

    metodos

--- ProyectoTPO2C-ViernesVirtual-2024/uade/edu/ar/AlgoritmoDeBlockchainImpl.java
+++ ProyectoTPO2C-ViernesVirtual-2024/uade/edu/ar/AlgoritmoDeBlockchainImpl.java
@@ -4,11 +4,14 @@ import uade.edu.progra3.AlgoritmoDeBlockchain;
 import uade.edu.progra3.model.Bloque;
 import uade.edu.progra3.model.Transaccion;
 
+
+import java.util.ArrayList;
 import java.util.List;
 /**
  * @author AlejandroFoglino
  */
 public class AlgoritmoDeBlockchainImpl implements AlgoritmoDeBlockchain {
+
 
     @Override
     public List<List<Bloque>> construirBlockchain(List<Transaccion> transacciones,
@@ -16,6 +19,46 @@ public class AlgoritmoDeBlockchainImpl implements AlgoritmoDeBlockchain {
                                                   int maxValorBloque,
                                                   int maxTransacciones,
                                                   int maxBloques) {
+        List<List<Bloque>>soluciones= new ArrayList<>();
+        List<Bloque>bloquesActuales = new ArrayList<>();
+        List<Transaccion> bloqueActual = new ArrayList<>();
+
         return null;
+    }
+    private void construirBLockchainRecurisvo(List<Transaccion> transaccions,int maxTamanioBloque,int maxValorBloque,
+                                              int maxTransacciones, int indice,List<Transaccion> bloqueActual,
+                                              List<List<Bloque>> soluciones,List<Bloque>bloquesActuales){
+        if(esBloequeValido())
+
+
+    }
+    private boolean esBloequeValido(List<Transaccion> bloque, int maxTamanioBloque,int maxValorBloque,
+                                    int maxTransacciones){ // Analizo el bloque entero
+        int tamnioTotal = calcularTamanioTotal(bloque);
+        int valorTotal = calcularValorTotal(bloque);
+        int numeTransacciones = bloque.size();
+
+    }
+    private int calcularTamanioTotal(List<Transaccion> bloque){
+        int tamnioTotal = 0;
+        for (Transaccion transaccion : bloque){
+            tamnioTotal += transaccion.getTamanio();
+        }
+        return tamnioTotal;
+    }
+    private int calcularValorTotal(List<Transaccion> bloque){
+        int valorTotal = 0;
+        for (Transaccion transaccion : bloque){
+            valorTotal += transaccion.getValor();
+        }
+        return valorTotal;
+    }
+    private boolean sePuedeAgregar(Transaccion transaccion, List<Transaccion> bloque){
+        for (Transaccion dependencia : transaccion.getDependencia()){
+            if(!bloque.contains(dependencia)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
