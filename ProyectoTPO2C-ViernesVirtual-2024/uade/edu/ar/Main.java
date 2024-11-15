b855e93250cb76796c36283758feadc1231f23d9
Fgrimoldi <grimoldifacundo@gmail.com>
2024-11-15 17:50:25 -0300

    cambio en la creacion de transacciones

--- ProyectoTPO2C-ViernesVirtual-2024/uade/edu/ar/Main.java
+++ ProyectoTPO2C-ViernesVirtual-2024/uade/edu/ar/Main.java
@@ -10,28 +10,33 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         AlgoritmoDeBlockchain algoritmoDeBlockchain = new AlgoritmoDeBlockchainImpl();
-        List<Transaccion> transacciones = TransaccionUtils.crearTransaccionesSimples(10, 100, 1000, 5);
+        List<Transaccion> transacciones = TransaccionUtils.crearTransaccionesConDependencias(10, 100, 1000, 5);
+        TransaccionUtils.firmarTransacciones(transacciones);
         List<List<Bloque>> blockchain = algoritmoDeBlockchain.construirBlockchain(transacciones, 1000, 10, 5, 5);
 
+        if (blockchain.isEmpty()){
+            System.out.println("No se generaron cadenas de bloques validas. ");
+        } else {
         // Mostrar bloques por pantalla
-        for (int i = 0; i < blockchain.size(); i++) {
-            System.out.println("Cadena de bloques " + (i + 1) + ":");
-            List<Bloque> cadena = blockchain.get(i);
+            for (int i = 0; i < blockchain.size(); i++) {
+                System.out.println("Cadena de bloques " + (i + 1) + ":");
+                List<Bloque> cadena = blockchain.get(i);
 
-            for (int j = 0; j < blockchain.size(); j++) {
-                Bloque bloque = cadena.get(j);
-                System.out.println(" Bloque " + (j + 1) + ":");
-                System.out.println("     Tamaño total: " + bloque.getTamanioTotal());
-                System.out.println("     Valor total:  " + bloque.getValorTotal());
-                System.out.println("      Transacciones:");
+                for (int j = 0; j < blockchain.size(); j++) {
+                    Bloque bloque = cadena.get(j);
+                    System.out.println("   Bloque " + (j + 1) + ":");
+                    System.out.println("        Tamaño total: " + bloque.getTamanioTotal());
+                    System.out.println("        Valor total:  " + bloque.getValorTotal());
+                    System.out.println("        Transacciones: ");
 
-                for (Transaccion transaccion : bloque.getTransacciones()) {
-                    System.out.println("       Tamaño: " + transaccion.getTamanio() +
-                            ", Valor: " + transaccion.getValor());
+                    for (Transaccion transaccion : bloque.getTransacciones()) {
+                        System.out.println("       Tamaño: " + transaccion.getTamanio() +
+                                ", Valor: " + transaccion.getValor());
 
+                    }
                 }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 }
